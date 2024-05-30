cd7442e22cb29854931751b4d0622537021105b5
M4raa <davidmarpons@gmail.com>
2024-05-30 08:17:24 +0200

    Main Updated

--- src/main/java/jpaswing/projectspotiy/app/Main.java
+++ src/main/java/jpaswing/projectspotiy/app/Main.java
@@ -1,7 +1,9 @@
 package jpaswing.projectspotiy.app;
 
 import jpaswing.projectspotiy.utilities.SearchMethods;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import java.io.IOException;
 import java.util.Scanner;
@@ -9,15 +11,9 @@ import java.util.Scanner;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) throws IOException {
-        //new SpringApplicationBuilder(Main.class)
-        //        .headless(false)
-        //        .web(WebApplicationType.NONE)
-        //        .run(args);
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("nombre:");
-        String name = sc.nextLine();
-        SearchMethods searchMethods = new SearchMethods();
-        searchMethods.grandSearch(name);
+        new SpringApplicationBuilder(Main.class)
+                .headless(false)
+                .web(WebApplicationType.NONE)
+                .run(args);
     }
 }
